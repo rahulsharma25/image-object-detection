ff458442ea3147323b70316178f9e211c967fc68
rahulsharma25 <rahulsharmagbr@gmail.com>
2022-02-24 17:32:39 +0530

    _

--- predict.py
+++ predict.py
@@ -12,26 +12,43 @@ if __name__ == "__main__":
     NMS_THRESHOLD = 0.4
 
 
-    # Select the image to make prediction on
-    img = cv.imread("images/4.jpg")
-    img_height, img_width = img.shape[:2]
-
-
     # Load the yolo model using pretrained weights and model configurations.
     model = cv.dnn.readNet('yolov3.weights', 'yolov3.cfg')
 
-    
+
     # Get those layers which are giving the output.
     # In yolo v3 model, there are 3 such output layers. 
     # These 3 output layers help in easily detecting objects of small and large sizes. 
     outputLayers = model.getUnconnectedOutLayersNames()
 
 
-    # Using the yolo model trained on COCO dataset.
-    # It contains 80 classes of objects.
+    # I am using the yolo version 3 model trained on COCO dataset.
+    # It contains 80 classes.
     classes = []
     with open('coco_names.txt', 'r') as name_file:
         classes = name_file.read().splitlines()
+
+
+    # Classifying the objects into various categories to segment the video based
+    # on the class which is detected with maximum confidence.
+    birds_and_animals = ['birds_and_animals', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe']
+    people = ['people', 'person']
+    vehicles = ['vehicles', 'bicycle', 'car', 'motorbike', 'aeroplane', 'bus', 'train', 'truck', 'boat']
+    road_objects = ['road_objects', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench']
+    toys_and_sports = ['toys_and_sports', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket']
+    apparels = ['apparels', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase']
+    dining = ['dining', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl']
+    eatables = ['eatables', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake']
+    furniture = ['furniture', 'chair', 'sofa', 'pottedplant', 'bed', 'diningtable']
+    electronics = ['electronics', 'tvmonitor', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'refrigerator', 'hair drier']
+    home_objects = ['home_objects', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'toothbrush']
+
+    global_ls = [birds_and_animals, people, vehicles, road_objects, toys_and_sports, apparels, dining, eatables, furniture, electronics, home_objects]
+
+
+    # Select the image to make prediction on
+    img = cv.imread("images/4.jpg")
+    img_height, img_width = img.shape[:2]
 
 
     # Change the image to a format which is required as yolo model input.
@@ -83,20 +100,29 @@ if __name__ == "__main__":
     final_indices = cv.dnn.NMSBoxes(bboxes, class_confidences, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
 
 
-    # Finally draw the final bounding boxes around detected objects.
+    # Draw the final bounding boxes around detected objects and tell their categories.
+    categories = []
     for i in final_indices:
         box = bboxes[i]
         object_class = classes[class_ids[i]]
+        for ls in global_ls:
+            if (object_class in ls) and (not(ls[0] in categories)):
+                categories.append(ls[0])
+                break
+
         confidence = class_confidences[i] * 100
         confidence = round(confidence, 1)
-
-        print(confidence)
 
         x, y, w, h = box[:]
         
         cv.putText(img, f'{object_class} {confidence}', (x, y-5), cv.FONT_HERSHEY_PLAIN, 0.8, (0, 255, 0), 1)
-        
         cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 255), 2)
+
+    if len(categories) > 0:
+        print('Categories present in image:', end=" ")
+        print(categories[0], end="")
+        for i in range (1, len(categories)):
+            print(f', {categories[i]}', end="")
 
 
     cv.imshow("img", img)
